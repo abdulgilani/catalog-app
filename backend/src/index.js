6f81e4872e3589e9ebf1bf85dbf53d6331c4f20b
abdulgilani <ah-gilani@hotmail.com>
2026-05-01 15:20:26 -0400

    Ready for initial deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
 
 import notesRoute from "./routes/noteRoutes/noteRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -9,17 +10,28 @@ import { connectDB } from "./config/db.js";
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(5001, () => {
